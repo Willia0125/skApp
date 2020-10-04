6d34bf344642382931442a0306ad526be0764e90
Aaron Williams <aaronwilliamz@gmail.com>
2020-10-04 14:34:53 -0500

    Added Maps to the App

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ViewChild, ElementRef} from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  infoWindows= [];
+  markers: any = [
+  {
+    title: "Scott's Kitchen",
+    latitude: "39.310100",
+    longitude: "-94.682570"
+  }
+  ];
+
   constructor() {}
 
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(39.310100, -94.6829);
+    const options = {
+      center: location,
+      zoom: 18,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+    }
 }
